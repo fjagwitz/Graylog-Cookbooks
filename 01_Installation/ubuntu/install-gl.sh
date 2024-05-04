373efe814dcdca4ce9e29f4e7f4f04e94d2fdcb3
Friedrich von Jagwitz <friedrich@jagwitz.net>
2024-05-04 09:52:24 +0200

    installer

---
+++ 01_Installation/ubuntu/install-gl.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sourcerepo="https://github.com/fjagwitz/Graylog-Cookbooks.git"
+targetrepo="/opt"
+
+mkdir "/opt/graylog/archives" "/opt/graylog/contentpacks" "/opt/graylog/maxmind" "/opt/graylog/nginx" "/opt/graylog/prometheus"
