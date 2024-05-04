01bebd5440e010a3391ac5150e9da4780e8e7c0f
Friedrich von Jagwitz <friedrich@jagwitz.net>
2024-05-04 12:58:02 +0200

    few changes

--- install-gl.sh
+++ install-gl.sh
@@ -5,20 +5,23 @@ targetrepo="/opt"
 
 # Create required Folders in the Filesystem
 sudo mkdir -p /opt/opensearch/{datanode1,datanode2,datanode3}
-sudo mkdir -p /opt/graylog/{archives,contentpacks,journal,maxmind,nginx,notifications,prometheus}
+sudo mkdir -p ${GL_GRAYLOG}/{archives,contentpacks,journal,maxmind,nginx,notifications,prometheus}
 
 # Create Environment Variables
 environmentfile="/etc/environment"
 
-echo "GL_OPENSEARCH_DATA=\"/opt/opensearch\"" | sudo tee -a ${environmentfile}
-echo "GL_GRAYLOG_ARCHIVES=\"/opt/graylog/archives\"" | sudo tee -a ${environmentfile}
-echo "GL_GRAYLOG_CONTENTPACKS=\"/opt/graylog/contentpacks\"" | sudo tee -a ${environmentfile}
-echo "GL_GRAYLOG_JOURNAL=\"/opt/graylog/journal\"" | sudo tee -a ${environmentfile}
-echo "GL_GRAYLOG_MAXMIND=\"/opt/graylog/maxmind\"" | sudo tee -a ${environmentfile}
-echo "GL_GRAYLOG_NGINX=\"/opt/graylog/nginx\"" | sudo tee -a ${environmentfile}
-echo "GL_GRAYLOG_NOTIFICATIONS=\"/opt/graylog/notifications\"" | sudo tee -a ${environmentfile}
-echo "GL_GRAYLOG_PROMETHEUS=\"/opt/graylog/prometheus\"" | sudo tee -a ${environmentfile}
+echo "GL_GRAYLOG=\"/opt/graylog\"" | sudo tee -a ${environmentfile}
+source ${environmentfile}
 
+echo "GL_GRAYLOG_ARCHIVES=\"${GL_GRAYLOG}/archives\"" | sudo tee -a ${environmentfile}
+echo "GL_GRAYLOG_CONTENTPACKS=\"${GL_GRAYLOG}/contentpacks\"" | sudo tee -a ${environmentfile}
+echo "GL_GRAYLOG_JOURNAL=\"${GL_GRAYLOG}/journal\"" | sudo tee -a ${environmentfile}
+echo "GL_GRAYLOG_MAXMIND=\"${GL_GRAYLOG}/maxmind\"" | sudo tee -a ${environmentfile}
+echo "GL_GRAYLOG_NGINX=\"${GL_GRAYLOG}/nginx\"" | sudo tee -a ${environmentfile}
+echo "GL_GRAYLOG_NOTIFICATIONS=\"${GL_GRAYLOG}/notifications\"" | sudo tee -a ${environmentfile}
+echo "GL_GRAYLOG_PROMETHEUS=\"${GL_GRAYLOG}/prometheus\"" | sudo tee -a ${environmentfile}
+
+echo "GL_OPENSEARCH_DATA=\"/opt/opensearch\"" | sudo tee -a ${environmentfile}
 source ${environmentfile}
 
 # Set Folder permissions
@@ -30,8 +33,11 @@ sudo wget -P ${GL_GRAYLOG_MAXMIND} https://github.com/P3TERX/GeoLite.mmdb/raw/do
 sudo wget -P ${GL_GRAYLOG_MAXMIND} https://github.com/P3TERX/GeoLite.mmdb/raw/download/GeoLite2-Country.mmdb
 
 # Move Files into the proper directories
-sudo mv ${installpath}/nginx/*.conf ${GL_GRAYLOG_NGINX}
-sudo mv ${installpath}/docker-compose.yaml /opt/graylog
-sudo mv ${installpath}/env.example /opt/graylog/.env
+sudo cp ${installpath}/nginx/*.conf ${GL_GRAYLOG_NGINX}
+sudo cp ${installpath}/docker-compose.yaml /opt/graylog
+sudo cp ${installpath}/env.example ${GL_GRAYLOG}/.env
+
+# Start Graylog
+sudo docker compose -f ${GL_GRAYLOG}/docker-compose.yml up -d
 
 
