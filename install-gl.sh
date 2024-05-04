2ae415fecec53901cc958a54ba857b8cdd8930ec
Friedrich von Jagwitz <friedrich@jagwitz.net>
2024-05-04 10:32:14 +0200

    path

--- install-gl.sh
+++ install-gl.sh
@@ -3,10 +3,10 @@ sourcerepo="https://github.com/fjagwitz/Graylog-Cookbooks.git"
 targetrepo="/opt"
 
 # Create required Folders in the Filesystem
-mkdir   "/opt/graylog/archives" \
-        "/opt/graylog/contentpacks" \ 
-        "/opt/graylog/maxmind" \
-        "/opt/graylog/nginx" \
-        "/opt/graylog/prometheus" -p
+mkdir -p "/opt/graylog/archives" \
+         "/opt/graylog/contentpacks" \ 
+         "/opt/graylog/maxmind" \
+         "/opt/graylog/nginx" \
+         "/opt/graylog/prometheus" 
 
 # Create Environment Variables
