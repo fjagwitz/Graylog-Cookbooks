52aab2d01d51b2657c54c7a666e114be53c686f2
Friedrich von Jagwitz <friedrich@jagwitz.net>
2024-05-06 17:21:44 +0200

    update

--- 01_Installation/install-gl-v1.sh
+++ 01_Installation/install-gl-v1.sh
@@ -1,8 +1,45 @@
 #!/bin/bash
-#sudo apt install git vim 
+# Check Minimum Requirements on Linux Server
+numberCores=$(cat /proc/cpuinfo | grep processor | wc -l)
+randomAccessMemory=$(grep MemTotal /proc/meminfo | awk '{print $2/1024 }' | awk -F'.' '{print $1 }')
 
+if [[ $numberCores -lt 8 ]]
+then
+  echo "[INFO] - THIS SYSTEM DOES NOT HAVE ENOUGH CPU CORES - EXIT "
+  exit
+else
+  echo "[INFO] - CPU CHECK SUCCESSFUL: $numberCores CORES "
+fi
 
-installpath="/opt/installpath"
+if [[ $randomAccessMemory -lt 32000 ]]
+then
+  echo "[INFO] - THIS SYSTEM DOES NOT HAVE ENOUGH MEMORY - EXIT "
+  exit
+else
+  echo "[INFO] - MEMORY CHECK SUCCESSFUL: $randomAccessMemory MB "
+fi
+
+# Removing preconfigured Docker Installation from Ubuntu
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+# Adding Docker Repository
+sudo apt-get -qq update
+sudo apt-get -qq install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings >/dev/null
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc >/dev/null
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+sudo apt-get -qq update
+
+# Installing Docker on Ubuntu
+sudo apt-get -qq install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Configure temporary installpath
+installpath="/tmp/graylog"
 
 # Configure vm.max_map_count for Opensearch (https://opensearch.org/docs/2.13/install-and-configure/install-opensearch/index/#important-settings)
 echo "vm.max_map_count=262144" | sudo tee -a /etc/sysctl.conf >/dev/null
