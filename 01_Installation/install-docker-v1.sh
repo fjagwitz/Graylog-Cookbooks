e8413b482def967a3bc9388eef0f93223cb55180
Friedrich von Jagwitz <friedrich@jagwitz.net>
2024-05-06 18:30:39 +0200

    update

---
+++ 01_Installation/install-docker-v1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Removing preconfigured Docker Installation from Ubuntu (just in case)
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get -qq remove $pkg; done
+
+# Adding Docker Repository
+sudo apt-get -qq update
+sudo apt-get -qq install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings > /dev/null
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc > /dev/null
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get -qq update
+
+# Installing Docker on Ubuntu
+sudo apt-get -qq install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
