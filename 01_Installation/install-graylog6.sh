d0947d1ca895a2d652cd8bab7b9bc88f2295fd6b
Friedrich von Jagwitz <friedrich@jagwitz.net>
2024-05-27 18:27:22 +0200

    update

--- 01_Installation/install-graylog6.sh
+++ 01_Installation/install-graylog6.sh
@@ -163,8 +163,6 @@ do
   sleep 5s
 done
 
-clear
-
 echo "[INFO] - SYSTEM READY FOR TESTING "
 echo "[INFO] - CREDENTIALS STORED IN /opt/graylog/graylog_credentials.txt "
 echo "[INFO] - USER: \"${GL_GRAYLOG_ADMIN}\" || PASSWORD: \"${GL_GRAYLOG_PASSWORD}\" || CLUSTER-ID: $(curl -s $(hostname)/api | jq '.cluster_id' | tr a-z A-Z )" | tee ${GL_GRAYLOG}/your_graylog_credentials.txt
