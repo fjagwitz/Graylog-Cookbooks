cb199c2fced8e6e017faa0278b8597c2200ebf30
Friedrich <friedrich@jagwitz.de>
2026-02-18 14:44:21 +0100

    update

--- 01_Installation/install-graylog.sh
+++ 01_Installation/install-graylog.sh
@@ -365,7 +365,7 @@ function_installGraylogStack () {
 
     # Reformat all Windows files to DOS format
     echo "[INFO] - Reformatting files to DOS CRLF " | logger -p user.info -e -t GRAYLOG-INSTALLER
-    find ${GRAYLOG_PATH}/sources/ -type f -print0 | xargs -0 unix2dos -- 2>/dev/null >/dev/null
+    find ${GRAYLOG_PATH}/sources/binaries/ -type f -print0 | xargs -0 unix2dos -- 2>/dev/null >/dev/null
     find ${GRAYLOG_PATH}/lookuptables/ -type f -print0 | xargs -0 unix2dos -- 2>/dev/null >/dev/null
 
     # Adapting Permissions for proper access by the Opensearch Containers (1000:1000)
