675c708164fa25949b25f2893be14781637d6942
friedrich <friedrich@jagwitz.de>
2026-06-18 12:01:04 +0200

    update

--- 01_Installation/install-graylog.sh
+++ 01_Installation/install-graylog.sh
@@ -588,7 +588,7 @@ function_addDataNodesToCluster () {
     PROVISION_CA=$(curl -s http://localhost/api/generate -u "${TMP_ADMIN}":"${TMP_PASSWORD}" -X POST -H "X-Requested-By: localhost") >/dev/null
     FINISH_CA=$(curl -s http://localhost/api/status/finish-config -u "${TMP_ADMIN}":"${TMP_PASSWORD}" -X POST -H "X-Requested-By: localhost") >/dev/null
 
-    echo "Finish-CA: ${FINISH_CA}" | logger -p user.info -e -t GRAYLOG-INSTALLER
+    echo "[INFO] - FINISH-CA: ${FINISH_CA^^}" | logger -p user.info -e -t GRAYLOG-INSTALLER
 
     if [[ $(echo ${FINISH_CA} | jq -r .result) == "FINISHED" ]]
     then
